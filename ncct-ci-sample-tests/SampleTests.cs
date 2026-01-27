3605ba2170c6ac5f37c7d117348b69f3804024cb
Chris Knox <Christopher.Knox@Advocatehealth.org>
2026-01-27 14:28:13 -0500

    add gitignore and tests project

---
+++ ncct-ci-sample-tests/SampleTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Ncct.Blazor.Sample.Tests;
+
+public class BasicTests
+{
+    [Fact]
+    public void SimpleTest()
+    {
+        var result = 2 + 2;
+        Assert.Equal(4, result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 2)]
+    [InlineData(2, 3, 5)]
+    public void DataTests(int a, int b, int expected)
+    {
+        var result = a + b;
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("test", true)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    public void StringValidationTests(string? input, bool expectedResult)
+    {
+        var isValid = !string.IsNullOrEmpty(input);
+        Assert.Equal(expectedResult, isValid);
+    }
+
+    [Fact]
+    public void ListOperationsTest()
+    {
+        var list = new List<int> { 1, 2, 3 };
+        list.Add(4);
+        
+        Assert.Equal(4, list.Count);
+        Assert.Contains(3, list);
+    }
+}
